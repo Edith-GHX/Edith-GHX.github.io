4527f08ae4fce401194af80cb5a06781a0331fa2
Edith-GHX <67405895+Edith-GHX@users.noreply.github.com>
2024-02-21 09:33:17 +0800

    Update News.vue

--- src/components/News.vue
+++ src/components/News.vue
@@ -38,16 +38,17 @@
     </header>
 
     <div class="content ml-8 mr-8 mt-8" style="margin-bottom: 100px;">
-      <div class="row">
-        <div class="col-md-9 col-12 pa-0" style="display: flex;">
-          <div class="text-h6 col col-3 indigo--text" style="display: flex;">
-            <v-icon style="font-size: 16px" class="notranslate mr-2 mdi theme--light indigo--text">mdi-circle</v-icon>
-            2024-02 
-          </div>
-          <div class="text-h6 col col-9">
-            王帅教授及其团队科研成果进入 SSRN 下载排行量前十!
-          </div>
-        </div>
+<div class="row">
+<div class="col-md-9 col-12 pa-0" style="display: flex;">
+<div class="text-h6 col col-3 indigo--text" style="display: flex;">
+<v-icon style="font-size: 16px" class="notranslate mr-2 mdi theme--light indigo--text">mdi-circle</v-icon>
+2024-02 
+</div>
+<div class="text-h6 col col-9">
+王帅教授及其团队科研成果进入 SSRN 下载排行量前十!
+</div>
+</div>
+
         <div class="pa-0 col col-12">
           <hr class="v-divider theme--light">
         </div>
